d694f92262b79df8975251b6947f1cf489d1d008
SiGoBoy <kuzmenkofilaret1@gmail.com>
2025-09-18 00:47:57 +0300

    added CPackConfig.cmake

---
+++ CPackConfig.cmake
@@ -0,0 +1,11 @@
+include(InstallRequiredSystemLibraries)
+
+set(CPACK_PACKAGE_CONTACT "kuzmenkofilaret1@gmail.com")
+set(CPACK_PACKAGE_VERSION "1.0.0.0")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "State C++ Design Pattern")
+
+set(CPACK_RESOURCE_FILE_README ${CMAKE_CURRENT_SOURCE_DIR}/README.md)
+
+set(CPACK_DEBIAN_PACKAGE_NAME "state-dev")
+set(CPACK_DEBIAN_PACKAGE_PREDEPENDS "cmake >= 3.10")
+set(CPACK_DEBIAN_PACKAGE_RELEASE 1)
